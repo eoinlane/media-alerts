daf3619c9c823901c842cbd3fb643f177b5918ad
eoinlane <eoinlane@gmail.com>
2018-03-04 11:12:38 +0000

    added a comment

--- src/test/scala/com/dnb/aas/mediaalerts/SparkSessionTestWrapper.scala
+++ src/test/scala/com/dnb/aas/mediaalerts/SparkSessionTestWrapper.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.SparkSession
 trait SparkSessionTestWrapper {
 
   lazy val spark: SparkSession = {
+    // Create a spark session
     SparkSession.builder().master("local").appName("spark session").getOrCreate()
   }
 
