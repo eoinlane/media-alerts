393225092f7724603ed63559934aacb5e8bf638d
eoinlane <eoinlane@gmail.com>
2018-03-04 01:39:04 +0000

    Initial commit

---
+++ src/test/scala/com/dnb/aas/mediaalerts/SparkSessionTestWrapper.scala
@@ -0,0 +1,11 @@
+package com.dnb.aas.mediaalerts
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionTestWrapper {
+
+  lazy val spark: SparkSession = {
+    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+  }
+
+}
